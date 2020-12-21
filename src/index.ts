2462c2d4682b5277359207a3b72f1959e8b9106c
Manav Misra <manavm@visionwebsoft.com>
2020-12-21 13:14:27 -0600

    :bulb: Add comments

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,12 @@ const app = express();
 
 const PORT: number = Number(process.env.PORT) || 3000;
 
+// App config
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+// Activate server
 app.listen(PORT, () => {
   console.log("Server 🏃🏾‍♂️", PORT);
 });
